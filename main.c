381dc4d8f06b6a9e08735c6d7c0fe6d42b3a940a
k311322 <105107717+k311322@users.noreply.github.com>
2023-01-13 09:14:44 +0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello World.\n");
+
+    printf("My first line of coding.\n");
+
+    printf("It is being put into the system.\n");
+
+    printf("The more we work. The more we can put into it the succeed part.\n");
+
+    printf("-----\n");
+
+    char characterName[] = "George";
+
+    int characterAge = 50;
+
+    printf("There was a man named %s.\n", characterName);
+    printf("He was %d years old.\n", characterAge);
+    printf("He really liked his own name which was %s and so his character as well.\n", characterName);
+    printf("But he did not like his own time and age which was %d.\n", characterAge);
+
+    printf("-----\n");
+
+    printf("My favorite %s is %d\n", "number", 500);
+
+    printf("-----\n");
+
+    int favNum = 90;
+
+    printf("My favorite %s is %d\n", "number", favNum);
+
+    printf("-----\n");
+
+    printf("-----\n");
+
+    printf("-----\n");
+
+    printf("     *     \n");
+    printf("    ***    \n");
+    printf("   *****   \n");
+    printf("  *******  \n");
+    printf(" ********* \n");
+    printf("***********\n");
+
+    return 0;
+}
+
+
